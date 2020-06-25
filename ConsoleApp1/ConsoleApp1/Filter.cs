1110a3b3365e3f2796cb62a77278925a29d71ac3
nici469 <nosaosayande@rocketmail.com>
2020-06-25 04:46:06 +0400

    Filter class constructor initialises Sobel Filter

--- ConsoleApp1/ConsoleApp1/Filter.cs
+++ ConsoleApp1/ConsoleApp1/Filter.cs
@@ -8,5 +8,36 @@ namespace ConsoleApp1
 {
     class Filter
     {
+        /// <summary>
+        /// the X-direction Sobel edge filter
+        /// </summary>
+        public double[,] Sobel_x;
+
+        /// <summary>
+        /// the y-direction Sobel edge filter
+        /// </summary>
+        public double[,] Sobel_y;
+
+        /// <summary>
+        /// initialises the Sobel filters for now
+        /// </summary>
+        public Filter()
+        {
+            double[,] Sx = new double[3, 3];
+            Sx[0, 0] = -1; Sx[1, 0] = 0; Sx[2, 0] = 1;
+            Sx[0, 1] = -1; Sx[1, 1] = 0; Sx[2, 1] = 1;
+            Sx[0, 2] = -1; Sx[1, 2] = 0; Sx[2, 2] = 1;
+
+            Sobel_x = Sx;
+
+            double[,] Sy = new double[3, 3];
+            Sy[0, 0] = -1; Sy[1, 0] = -2; Sy[2, 0] = -1;
+            Sy[0, 1] = 0; Sy[1, 1] = 0; Sy[2, 1] = 0;
+            Sy[0, 2] = 1; Sy[1, 2] = 2; Sy[2, 2] = 1;
+
+            Sobel_y = Sy;
+        }
+
     }
+
 }
